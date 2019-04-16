9b00b80c0439e50fcb5acd03fdb21afaecc03901
Bartosz Bełcik <bartosz.belcik@gmail.com>
2019-04-16 12:51:21 +0200

    #1 Add indication of under-/over- time

--- workreport.sh
+++ workreport.sh
@@ -14,14 +14,31 @@
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
 
-total_minutes=0
+dir="${1:-$HOME/.worklog/$(date +%Y)/$(( $(date +%m) ))}"
 
-for day in *; do
+if [[ ! -d "$dir" ]]; then
+    echo "TOTAL: 0"
+    exit
+fi
+
+cd "$dir"
+
+total_minutes=0
+days=0
+for day in $(ls | sort -n); do
+    days=$(( days + 1 ))
 	minutes=`cat $day | uniq | wc -l`
 	total_minutes=$((total_minutes+minutes))
 	
 	day_hours=$((minutes/60))
 	day_part_hours=$((minutes%60))
-	printf "%s %d:%02d\n" $day $day_hours $day_part_hours
+	printf "%2d %d:%02d\n" $day $day_hours $day_part_hours
 done
-printf "TOTAL: %d:%02d" $((total_minutes/60)) $((total_minutes%60))
+printf "TOTAL: %4d:%02d\n" $((total_minutes/60)) $((total_minutes%60))
+delta=$(( total_minutes - (days * 8 * 60) ))
+prefix="+"
+if (( delta < 0 )); then
+    delta=$(( -delta ))
+    prefix="-"
+fi
+printf "    Δ: %4d:%02d\n" "$prefix$(( delta / 60 ))" $(( delta % 60))
